17d7fa96ee4a02ae09f3515da73b4befdeea9d54
Kenneth Pierce <knyrii@gmail.com>
2013-10-07 16:40:32 -0400

    pagination function

--- lib/lib/ml/html_helper.php
+++ lib/lib/ml/html_helper.php
@@ -7,3 +7,31 @@ function combine_attrib(array $attrib=null){
 	}
 	return trim($res);
 }
+function paginate($totalRows,$currentRow,$rowsPerPage,$extra='',$prefix=''){
+	$cPages=ceil($totalRows/$rowsPerPage);
+	if($cPages == 1){return ' ';}
+	$pageLinks = '';
+	if ($currentRow > 0) {
+		$pageLinks .= '<a href="?'.$prefix.'start=0&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'"><img src="/lib/i/resultset_first.png" alt="first page"></a>';
+		$pageLinks .= ' <a href="?'.$prefix.'start='.($currentRow-$rowsPerPage).'&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'"><img src="/lib/i/resultset_previous.png" alt="previous page"></a>';
+	}
+	$page = $currentRow/$rowsPerPage;
+	$cPage = $page;
+	$topPage = $page+2;
+	$page -= 2;
+	if ($page < 0) { $page = 0; }
+	for (; $page <= $topPage && $page < $cPages; $page++) {
+		if ($page == $cPage)
+			$pageLinks .= ' '.($page+1);
+		else
+			$pageLinks .= ' <a href="?'.$prefix.'start='.($page*$rowsPerPage).'&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'">'.($page+1).'</a>';
+	}
+
+	if ($topPage-1 < $cPages) {
+		if ($topPage < $cPages-1)
+			$pageLinks .= ' ... <a href="?'.$prefix.'start='.(($cPages-1)*$rowsPerPage).'&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'">'.$cPages.'</a>';
+		$pageLinks .= ' <a href="?'.$prefix.'start='.(($topPage-1)*$rowsPerPage).'&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'"><img src="/lib/i/resultset_next.png" alt="next page"></a>';
+		$pageLinks .= ' <a href="?'.$prefix.'start='.(($cPages-1)*$rowsPerPage).'&amp;'.$prefix.'rpp='.$rowsPerPage.$extra.'"><img src="/lib/i/resultset_last.png" alt="last page"></a>';
+	}
+	return $pageLinks;
+}
