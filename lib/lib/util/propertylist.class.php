e307daa2a65cf7f7f5b21bbbacdbdcfbe4460651
knyri <knyri@github>
2025-01-02 14:54:08 -0500

    Can ignore changes certain keys

--- lib/lib/util/propertylist.class.php
+++ lib/lib/util/propertylist.class.php
@@ -8,20 +8,20 @@
  * @author Ken
  */
 class PropertyList{
-	protected $data=array();
+	protected $data= array();
 	/**
 	 * @param string $key
 	 * @param mixed $value
 	 */
-	public function set($key,$value){
-		$this->data[$key]=$value;
+	public function set($key, $value){
+		$this->data[$key]= $value;
 	}
 	/**
 	 * @param string $key
 	 * @param mixed $default
 	 * @return mixed
 	 */
-	public function get($key,$default=null){
+	public function get($key, $default= null){
 		if(array_key_exists($key, $this->data)){
 			return $this->data[$key];
 		}
@@ -45,17 +45,17 @@ class PropertyList{
 	 * @param array $list $key=>$value list
 	 */
 	public function setAll(array $list){
-		$this->data=array_merge($this->data,$list);
+		$this->data= array_merge($this->data, $list);
 	}
 	public function clear(){
-		$this->data=array();
+		$this->data= array();
 	}
 	/**
 	 * Sets the internal array to the supplied array.
 	 * @param array $a
 	 */
 	public function initFrom(array $a){
-		$this->data=$a;
+		$this->data= $a;
 	}
 	public function count(){
 		return count($this->data);
@@ -75,13 +75,38 @@ class PropertyList{
 class ChangeTrackingPropertyList extends PropertyList{
 	protected
 		$changes= array(),
-		$cleared= array();
+		$ignoredChanges= array(),
+		$cleared= array(),
+		// props in this list don't count torwards the overall hasChanges
+		$ignored= array()
+	;
+	public function setIgnoredProps($ary){
+		$this->ignored= $ary;
+	}
+	public function discardChanges(){
+		$this->changes= array();
+		$this->ignoredChanges= array();
+		$this->cleared= array();
+	}
 	public function discardChange($key){
 		if(array_key_exists($key, $this->changes)){
 			unset($this->changes[$key]);
 		}else if (array_key_exists($key, $this->cleared)){
 			unset($this->cleared[$key]);
+		}else if (array_key_exists($key, $this->ignoredChanges)){
+			unset($this->ignoredChanges[$key]);
 		}
+	}
+	/**
+	 * @deprecated use commitChanges()
+	 */
+	public function mergeChanges(){
+		$this->commitChanges();
+	}
+	public function commitChanges(){
+		if(!$this->hasChanges()) return;
+		$this->data= $this->getFinal();
+		$this->discardChanges();
 	}
 	public function commitChange($key){
 		if(array_key_exists($key, $this->changes)){
@@ -90,23 +115,30 @@ class ChangeTrackingPropertyList extends PropertyList{
 		}else if (array_key_exists($key, $this->cleared)){
 			unset($this->cleared[$key]);
 			unset($this->data[$key]);
+		}else if (array_key_exists($key, $this->ignoredChanges)){
+			$this->data[$key]= $this->ignoredChanges[$key];
+			unset($this->ignoredChanges[$key]);
 		}
 	}
 	public function hasChanges(){
 		return count($this->changes) > 0 || count($this->cleared) > 0;
 	}
 	public function hasChanged($key){
-		return array_key_exists($key, $this->changes) || array_key_exists($key, $this->cleared);
+		return array_key_exists($key, $this->changes) || array_key_exists($key, $this->cleared) || array_key_exists($key, $this->ignoredChanges);
 	}
 	public function getChanges(){
-		$changes=array('old'=>array(),'new'=>array());
-		foreach($this->changes as $k=>$v){
-			$changes['new'][$k]=$v;
-			$changes['old'][$k]=array_key_exists($k,$this->data)?$this->data[$k]:null;
+		$changes= array('old'=> array(),'new'=> array());
+		foreach($this->changes as $k => $v){
+			$changes['new'][$k]= $v;
+			$changes['old'][$k]= array_key_exists($k, $this->data) ? $this->data[$k] : null;
 		}
-		foreach($this->cleared as $k=>$v){
-			$changes['new'][$k]=null;
-			$changes['old'][$k]=array_key_exists($k,$this->data)?$this->data[$k]:null;
+		foreach($this->cleared as $k => $v){
+			$changes['new'][$k]= null;
+			$changes['old'][$k]= array_key_exists($k, $this->data) ? $this->data[$k] : null;
+		}
+		foreach($this->ignoredChanges as $k => $v){
+			$changes['new'][$k]= $v;
+			$changes['old'][$k]= array_key_exists($k, $this->data) ? $this->data[$k] : null;
 		}
 		return $changes;
 	}
@@ -118,20 +150,21 @@ class ChangeTrackingPropertyList extends PropertyList{
 		parent::initFrom($a);
 	}
 	public function reset(){
-		$this->changes=array();
-		$this->data=array();
-		$this->cleared=array();
+		$this->changes= array();
+		$this->data=    array();
+		$this->cleared= array();
+		$this->ignoredChanges= array();
 	}
 	public function clear(){
 		foreach($this->data as $k => $v){
-			$this->cleared[$k]=true;
+			$this->cleared[$k]= true;
 		}
 	}
 	/**
 	 * @param string $k
 	 * @param mixed $v
 	 */
-	public function set($k,$v){
+	public function set($k, $v){
 		if(
 			// exists
 			array_key_exists($k, $this->data) &&
@@ -141,27 +174,27 @@ class ChangeTrackingPropertyList extends PropertyList{
 			(null === $this->data[$k]) == (null === $v)
 		){
 			unset($this->changes[$k]);
+			unset($this->ignoredChanges[$k]);
 			return;
 		}
-		$this->changes[$k]=$v;
+		if(array_search($k, $this->ignored) !== false){
+			$this->ignoredChanges[$k]= $v;
+		}else{
+			$this->changes[$k]= $v;
+		}
 		unset($this->cleared[$k]);
-	}
-	public function discardChanges(){
-		$this->changes=array();
-		$this->cleared=array();
-	}
-	public function mergeChanges(){
-		$this->data=$this->getFinal();
-		$this->discardChanges();
 	}
 	/**
 	 * @param string $key
 	 * @param mixed $default
 	 * @return mixed
 	 */
-	public function get($key,$default=null){
+	public function get($key, $default= null){
 		if(array_key_exists($key, $this->changes)){
 			return $this->changes[$key];
+		}
+		if(array_key_exists($key, $this->ignoredChanges)){
+			return $this->ignoredChanges[$key];
 		}
 		if(array_key_exists($key, $this->data) && !isset($this->cleared[$key])){
 			return $this->data[$key];
@@ -174,7 +207,7 @@ class ChangeTrackingPropertyList extends PropertyList{
 	 * @param string $default
 	 * @return mixed
 	 */
-	public function getPrevious($key,$default=null){
+	public function getPrevious($key, $default= null){
 		if(array_key_exists($key,$this->data)){
 			return $this->data[$key];
 		}
@@ -185,8 +218,9 @@ class ChangeTrackingPropertyList extends PropertyList{
 	 */
 	public function uset($k){
 		unset($this->changes[$k]);
+		unset($this->ignoredChanges[$k]);
 // 		unset($this->data[$k]);
-		$this->cleared[$k]=true;
+		$this->cleared[$k]= true;
 	}
 	/**
 	 * Copies the data held by the internal array to the given array.
@@ -197,7 +231,7 @@ class ChangeTrackingPropertyList extends PropertyList{
 		return array_replace($ary, $this->getFinal());
 	}
 	protected function getFinal(){
-		return array_diff_key(array_merge($this->data,$this->changes),$this->cleared);
+		return array_diff_key(array_merge($this->data, $this->changes, $this->ignoredChanges), $this->cleared);
 	}
 	/**
 	 * @param array $list $key=>$value list
