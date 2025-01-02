57ebc4fce6eceee5b4db1cd283d4250cc3aabefb
knyri <knyri@github>
2025-01-02 15:22:26 -0500

    better error catching and debug version

--- lib/lib/net/ldap.class.php
+++ lib/lib/net/ldap.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * LDAP connection wrapper
-*/
+ */
 class Ldap {
 	private
 		$con,
@@ -36,54 +36,69 @@ class Ldap {
 		}
 	}
 	public function errHandler($number, $string, $file, $line, $context){
-		$error= explode(':', $string, 3);
 		$this->caughtError= true;
-		$this->lastError= trim($error[1]);
+		$this->lastError= trim($string);
 		if(function_exists('logit')){
-			logit("$string\n$file\n$line\n".print_r($context, true));
+			logit("($number)$string(".$this->getErrorNum().") in $file($line)");//\n".var_dump_ret($context));
 		}
+	}
+	/**
+	 * Returns the last error caught with the error handler. ldap functions like to throw warnings intead of errors...
+	 * @return boolean|string
+	 */
+	public function getCaughtError(){
+		return $this->caughtError ? $this->lastError : false;
 	}
 	/** The result from ldap_connect
 	 * @return boolean
 	 */
-	function isValid(){
+	public function isValid(){
 		return $this->con !== false;
 	}
 	/**
-	 * @param string $rdn
-	 * @param string $password
-	 * @return boolean
+	 * Log into a directory. See ldap_bind
+	 * @param string $rdn [user]@[domain]
+	 * @param string $password The password
+	 * @return boolean true on success, false on failure
 	 */
 	function bind($rdn= null, $password= null){
 		return ldap_bind($this->con, $rdn, $password);
 	}
+	/**
+	 * Log out of the directory. See ldap_unbind
+	 * @return boolean
+	 */
 	function unbind(){
 		return ldap_unbind($this->con);
 	}
+	/**
+	 * Starts TLS... See ldap_start_tls
+	 * @return boolean
+	 */
 	function startTls(){
 		return ldap_start_tls($this->con);
 	}
 	/**
-	 * Adds attributes to the DN
-	 * @param string $dn
-	 * @param array $entry
+	 * Adds attributes to the DN. See ldap_add
+	 * @param string $dn Distinguished name for the entry
+	 * @param array $entry entry properties
 	 * @return boolean
 	 */
 	function add($dn, array $entry){
 		return ldap_add($this->con, $dn, $entry);
 	}
 	/**
-	 * Tests to see if the entry's attribute has that value
-	 * @param string $dn
-	 * @param string $attribute
-	 * @param string $value
+	 * Tests to see if the entry's attribute has that value. See ldap_compare
+	 * @param string $dn Distinguished name of the entry to test
+	 * @param string $attribute Attribute to test
+	 * @param string $value Value to compare
 	 * @return mixed Returns TRUE if value matches otherwise returns FALSE. Returns -1 on error.
 	 */
 	function compare($dn, $attribute, $value){
 		return ldap_compare($this->con, $dn, $attribute, $value);
 	}
 	/**
-	 * Deletes an entry
+	 * Deletes an entry. See ldap_delete
 	 * @param string $dn
 	 * @return boolean
 	 */
@@ -91,21 +106,21 @@ class Ldap {
 		return ldap_delete($this->con, $dn);
 	}
 	/**
-	 * The last error
+	 * The last error. See ldap_error
 	 * @return string
 	 */
 	function getError(){
 		return ldap_error($this->con);
 	}
 	/**
-	 * The last error number
+	 * The last error number. See ldap_errno
 	 * @return number
 	 */
 	function getErrorNum(){
 		return ldap_errno($this->con);
 	}
 	/**
-	 * Sets the option
+	 * Sets the option. See ldap_set_option
 	 * @param number $option
 	 * @param mixed $value
 	 * @return boolean
@@ -114,6 +129,7 @@ class Ldap {
 		return ldap_set_option($this->con, $option, $value);
 	}
 	/**
+	 * See ldap_get_option
 	 * @param number $option
 	 * @param mixed $value
 	 * @return boolean
@@ -122,6 +138,7 @@ class Ldap {
 		return ldap_get_option($this->con, $option, $value);
 	}
 	/**
+	 * See ldap_rename
 	 * @param string $dn Entity to rename
 	 * @param string $newrdn The new RDN
 	 * @param string $newparent The new parent entry
@@ -140,8 +157,8 @@ class Ldap {
 	}
 	/**
 	 * Does not recurse
-	 * @param string|array $base_dn
-	 * @param string|array $filter
+	 * @param string $base_dn
+	 * @param string $filter
 	 * @param array $attributes
 	 * @param boolean $attrsonly
 	 * @param int $sizelimit
@@ -150,6 +167,7 @@ class Ldap {
 	 * @return boolean|LdapResult
 	 */
 	function listEntries($base_dn, $filter, $attributes= null, $attrsonly= null, $sizelimit= null, $timelimit= null, $deref= null){
+		$this->listenForErrors();
 		if($attributes){
 			if($attrsonly){
 				if($sizelimit){
@@ -171,17 +189,14 @@ class Ldap {
 		}else{
 			$res= ldap_list($this->con, $base_dn, $filter);
 		}
-		if(is_array($res)){
-			$this->result= array_map(function($e){return new LdapResult($this->con, $e);}, $res);
-		}else{
-			$this->result= $res === false ? false : new LdapResult($this->con, $res);
-		}
+		$this->stopListeningForErrors();
+		$this->result= $res === false ? false : new LdapResult($this->con, $res, $this->partialResults());
 		return $this->result;
 	}
 	/**
 	 * Searches $base_dn and recurses into child DNs
-	 * @param string|array $base_dn
-	 * @param string|array $filter
+	 * @param string $base_dn
+	 * @param string $filter
 	 * @param array $attributes
 	 * @param boolean $attrsonly
 	 * @param int $sizelimit
@@ -213,25 +228,41 @@ class Ldap {
 			$res= ldap_search($this->con, $base_dn, $filter);
 		}
 		$this->stopListeningForErrors();
-		if(is_array($res)){
-			$this->result= array_map(function($e){return new LdapResult($this->con, $e);}, $res);
-		}else{
-			$this->result= $res === false ? false : new LdapResult($this->con, $res, $this->lastError && strpos($this->lastError, 'Partial search results returned') === 0);
-		}
+		$this->result= $res === false ? false : new LdapResult($this->con, $res, $this->partialResults());
 		return $this->result;
 	}
 	/**
+	 * If the last search, list, or read returned no results
+	 * @return boolean
+	 */
+	public function noResults(){
+		if(!$this->result){
+			return true;
+		}
+		$res= false;
+		switch($this->getErrorNum()){
+			case 32:
+			case 94:
+				$res= true;
+			break;
+			case 0:
+				$res= $this->result->countEntries() === 0;
+		}
+		return $res;
+	}
+	/**
 	 * Read a single entry
-	 * @param string|array $base_dn
-	 * @param string|array $filter
+	 * @param string $base_dn
+	 * @param string $filter
 	 * @param array $attributes
 	 * @param boolean $attrsonly
 	 * @param int $sizelimit
 	 * @param int $timelimit
 	 * @param int $deref
-	 * @return boolean|LdapResult
+	 * @return boolean|LdapResult Note: can return false if the $base_dn does not exist
 	 */
 	function read($base_dn, $filter='(objectClass=*)', $attributes= null, $attrsonly= null, $sizelimit= null, $timelimit= null, $deref= null){
+		$this->listenForErrors();
 		if($attributes){
 			if($attrsonly){
 				if($sizelimit){
@@ -253,12 +284,69 @@ class Ldap {
 		}else{
 			$res= ldap_read($this->con, $base_dn, $filter);
 		}
-		if(is_array($res)){
-			$this->result= array_map(function($e){return new LdapResult($this->con, $e);}, $res);
-		}else{
-			$this->result= $res === false ? false : new LdapResult($this->con, $res);
-		}
+		$this->stopListeningForErrors();
+		$this->result= $res === false ? false : new LdapResult($this->con, $res, $this->partialResults());
 		return $this->result;
+	}
+	/**
+	 * If the last search, list, or read reached the max record limit
+	 * @return boolean
+	 */
+	public function partialResults(){
+		return Ldap::isPartialResults($this->getErrorNum());
+	}
+	public function errorIsSerious(){
+		return self::isSeriousError($this->getErrorNum());
+	}
+	public static function isSeriousError($errNum){
+		if($errNum == 0) return false;
+		if(self::isPartialResults($errNum)) return false;
+		return array_search($errNum, array(3,4,5,6,10,11,16,20,32,51,68,88,94)) === false;
+	}
+	public static function isServerSideError($errNum){
+		$res= false;
+		switch($errNum){
+			case 51:
+			case 52:
+			case 53:
+			case 76:
+			case 81:
+			case 85:
+			case 90:
+			case 92:
+			case 100:
+			case 101:
+				$res= true;
+		}
+		return $res;
+	}
+	public static function isAccessError($errNum){
+		$res= false;
+		switch($errNum){
+			case 7:
+			case 8:
+			case 13:
+			case 14:
+			case 48:
+			case 49:
+			case 50:
+			case 86:
+			case 123:
+				$res= true;
+		}
+		return $res;
+	}
+	public static function isPartialResults($errNum){
+		$res= false;
+		switch($errNum){
+			case 95:
+			case 113:
+			case 11:
+			case 4:
+			case 9:
+				$res= true;
+		}
+		return $res;
 	}
 }
 /**
@@ -266,11 +354,11 @@ class Ldap {
  */
 class LdapResult Implements IteratorAggregate{
 	private
-	$con,
-	$result,
-	$errcode,
-	$matcheddn,
-	$errmsg,
+		$con,
+		$result,
+		$errcode,
+		$matcheddn,
+		$errmsg,
 		$referrals= array(),
 		$sizeLimitHit
 	;
@@ -387,8 +475,9 @@ class LdapResult Implements IteratorAggregate{
 }
 class LdapObject{
 	protected
-	$con,
-	$dn;
+		$con,
+		$dn
+	;
 	function __construct($con, $dn){
 		$this->con= $con;
 		$this->dn= $dn;
@@ -476,7 +565,7 @@ class LdapResultIterator implements Iterator {
  */
 class LdapResultEntry extends LdapObject{
 	private
-	$entry,
+		$entry,
 		$reuse= true,
 		$attr= false
 	;
@@ -556,7 +645,7 @@ class LdapResultEntry extends LdapObject{
 	function getAttributes(){
 		if(!$this->attr){
 			$this->attr= ldap_get_attributes($this->con, $this->entry);
-	}
+		}
 		return $this->attr;
 	}
 	/**
@@ -730,4 +819,208 @@ class LdapAttribute implements Iterator{
 		return $this->next();
 	}
 
+}
+
+
+// DEBUG VERSION
+class LdapDebug extends Ldap{
+	private
+		$errHandler,
+		$caughtError= false,
+		$lastError= null
+	;
+	function __construct($host, $port= 389){
+		$this->errHandler= array($this, 'errHandler');
+		$this->listenForErrors();
+		parent::__construct($host, $port);
+		$this->stopListeningForErrors();
+	}
+	private function listenForErrors(){
+		$this->caughtError= false;
+		$this->lastError= false;
+		$this->originalErrHandler= set_error_handler($this->errHandler);
+	}
+	private function stopListeningForErrors(){
+		restore_error_handler();
+		if($this->caughtError){
+			$this->caughtError= false;
+		}
+	}
+	public function errHandler($number, $string, $file, $line, $context){
+		$this->caughtError= true;
+		$this->lastError= trim($string);
+		if(function_exists('logit')){
+			logit("$string in $file($line)\n".var_dump_ret($context));
+		}
+	}
+	public function getCaughtError(){
+		return $this->caughtError ? $this->lastError : false;
+	}
+	/**
+	 * @param string $rdn
+	 * @param string $password
+	 * @return boolean
+	 */
+	function bind($rdn= null, $password= null){
+		$this->listenForErrors();
+		$ret= parent::bind($rdn, $password);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	function unbind(){
+		$this->listenForErrors();
+		$ret= parent::unbind();
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	function startTls(){
+		$this->listenForErrors();
+		$ret= parent::startTls();
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Adds attributes to the DN
+	 * @param string $dn
+	 * @param array $entry
+	 * @return boolean
+	 */
+	function add($dn, array $entry){
+		$this->listenForErrors();
+		$ret= parent::add($dn, $entry);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Tests to see if the entry's attribute has that value
+	 * @param string $dn
+	 * @param string $attribute
+	 * @param string $value
+	 * @return mixed Returns TRUE if value matches otherwise returns FALSE. Returns -1 on error.
+	 */
+	function compare($dn, $attribute, $value){
+		$this->listenForErrors();
+		$ret= parent::compare($dn, $attribute, $value);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Deletes an entry
+	 * @param string $dn
+	 * @return boolean
+	 */
+	function delete($dn){
+		$this->listenForErrors();
+		$ret= parent::delete($dn);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * The last error
+	 * @return string
+	 */
+	function getError(){
+		$this->listenForErrors();
+		$ret= parent::getError();
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * The last error number
+	 * @return number
+	 */
+	function getErrorNum(){
+		$this->listenForErrors();
+		$ret= parent::getErrorNum();
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Sets the option
+	 * @param number $option
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	function setOption($option, $value){
+		$this->listenForErrors();
+		$ret= parent::setOption($option, $value);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * @param number $option
+	 * @param mixed $value
+	 * @return boolean
+	 */
+	function getOption($option, &$value){
+		$this->listenForErrors();
+		$ret= parent::getOption($option, $value);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * @param string $dn Entity to rename
+	 * @param string $newrdn The new RDN
+	 * @param string $newparent The new parent entry
+	 * @param boolean $deleteold If TRUE the old RDN value(s) is removed, else the old RDN value(s) is retained as non-distinguished values of the entry.
+	 * @return boolean
+	 */
+	function rename($dn, $newrdn, $newparent, $deleteold){
+		$this->listenForErrors();
+		$ret= parent::rename($dn, $newrdn, $newparent, $deleteold);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Does not recurse
+	 * @param string|array $base_dn
+	 * @param string|array $filter
+	 * @param array $attributes
+	 * @param boolean $attrsonly
+	 * @param int $sizelimit
+	 * @param int $timelimit
+	 * @param int $deref
+	 * @return boolean|LdapResult
+	 */
+	function listEntries($base_dn, $filter, $attributes= null, $attrsonly= null, $sizelimit= null, $timelimit= null, $deref= null){
+		$this->listenForErrors();
+		$ret= parent::listEntries($base_dn, $filter, $attributes, $attrsonly, $sizelimit, $timelimit, $deref);
+		$this->stopListeningForErrors();
+		return $ret;
+
+	}
+	/**
+	 * Searches $base_dn and recurses into child DNs
+	 * @param string|array $base_dn
+	 * @param string|array $filter
+	 * @param array $attributes
+	 * @param boolean $attrsonly
+	 * @param int $sizelimit
+	 * @param int $timelimit
+	 * @param int $deref
+	 * @return boolean|LdapResult
+	 */
+	function search($base_dn, $filter, $attributes= false, $attrsonly= null, $sizelimit= false, $timelimit= false, $deref= false){
+		$this->listenForErrors();
+		$ret= parent::search($base_dn, $filter, $attributes, $attrsonly, $sizelimit, $timelimit, $deref);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
+	/**
+	 * Read a single entry
+	 * @param string|array $base_dn
+	 * @param string|array $filter
+	 * @param array $attributes
+	 * @param boolean $attrsonly
+	 * @param int $sizelimit
+	 * @param int $timelimit
+	 * @param int $deref
+	 * @return boolean|LdapResult
+	 */
+	function read($base_dn, $filter='(objectClass=*)', $attributes= null, $attrsonly= null, $sizelimit= null, $timelimit= null, $deref= null){
+		$this->listenForErrors();
+		$ret= parent::read($base_dn, $filter, $attributes, $attrsonly, $sizelimit, $timelimit, $deref);
+		$this->stopListeningForErrors();
+		return $ret;
+	}
 }
